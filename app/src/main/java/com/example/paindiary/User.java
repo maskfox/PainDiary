dff2e7a663e3fb0cc1d0d44617cad47c736f1b26
maskfox <qhuu0012@student.monash.edu>
2021-05-15 00:08:09 +1000

    Initial commit

---
+++ app/src/main/java/com/example/paindiary/User.java
@@ -0,0 +1,19 @@
+package com.example.paindiary;
+
+public class User {
+    public String firstName, lastName, age, email;
+
+    public User(){
+
+    }
+
+    public User(String firstName, String lastName, String age, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+
+
+
+}
